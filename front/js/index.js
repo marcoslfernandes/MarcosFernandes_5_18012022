a0ff4f72835d456fa6ee88c0efab640e6f6a76c4
Marcos Fernandes <marcosformation@gmail.com>
2022-01-25 17:27:03 +0100

    Troisieme version

--- front/js/index.js
+++ front/js/index.js
@@ -1,24 +1,62 @@
+
 fetch("http://localhost:3000/api/products/")
-  .then(function(res) {
-    console.log(res);
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-    let items=document.getElementById("items");
-    value.forEach(item => {
-      let child=document.createElement("a");
-      child.href="./product.html?id="+item._id;
-      child.innerHTML='<article><img src="'+item.imageUrl+'" alt="'+item.altTxt+'" />'+
-      '<h3 class="productName">'+item.name+'</h3>'+
-      '<p class="productDescription">'+item.description+'</p>'+
-      '</article>';
-      items.appendChild(child);
-    });
-  })
-  .catch(function(err) {
-    console.log(err);
-  }); 
+.then(function(res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
+.then(function(products) {
+  console.log(products);
+  let items = document.getElementById("items")
+  for(i=0; i<products.length; i++){
+    items.innerHTML += `
+    
+    <a href="./product.html?id=${products[i]._id}">
+    <article><img src="${products[i].imageUrl}" />
+    <h3 class="productName">${products[i].name}</h3>
+    <p class="productDescription">${products[i].description}</p>
+    </article>
+    </a>
+    
+    `
+  }
+ 
+
+})
+
+products()
+
+
+
+
+
+
+
+
+// fetch("http://localhost:3000/api/products/")
+//   .then(function(res) {
+//     console.log(res);
+//     if (res.ok) {
+//       return res.json();
+//     }
+//   })
+//   .then(function(value) {
+//     console.log(value);
+//     let items=document.getElementById("items");
+//     value.forEach(item  => {
+//       // à quoi correspond le item ici ?
+//       let child=document.createElement("a");
+//       child.href="./product.html?id="+item._id;
+//       child.innerHTML='<article><img src="'+item.imageUrl+'" alt="'+item.altTxt+'" />'+
+//       '<h3 class="productName">'+item.name+'</h3>'+
+//       '<p class="productDescription">'+item.description+'</p>'+
+//       '</article>';
+//       items.appendChild(child);
+//       // pourquoi utiliser appendChild et pas append ?
+//     });
+//   })
+//   .catch(function(err) {
+//     // à quoi correspond le catch ?
+//     console.log(err);
+//   }); 
 
---
+++ front/js/products.js
@@ -0,0 +1,157 @@
+  let queryString = window.location.search;
+  let urlParams = new URLSearchParams(queryString);
+  let Id = urlParams.get('id');
+
+  let imgItem = document.getElementsByClassName("item__img")[0];
+  let titleItem = document.getElementById("title");
+  let priceItem = document.getElementById("price");
+  let descriptionItem = document.getElementById("description");
+  let colorsItem = document.getElementById("colors");
+
+
+  fetch("http://localhost:3000/api/products/"+Id)
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(product) {
+    console.log(product);
+
+  
+    imgItem.innerHTML += `<img src="${product.imageUrl}" />`
+    titleItem.innerHTML += `${product.name}`
+    priceItem.innerHTML += `${product.price}`
+    descriptionItem.innerHTML += `${product.description}`
+    
+    product.colors.forEach(element => {
+      colorsItem.innerHTML += `<option value="">${element}</option>`
+    });
+    
+ 
+
+  })
+
+
+product()
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Storage.prototype.setObj = function(key, obj) {
+//     return this.setItem(key, JSON.stringify(obj))
+//   } 
+//   Storage.prototype.getObj = function(key) {
+//     return JSON.parse(this.getItem(key))
+//   }
+  
+//   //gestion de l'url
+//   let queryString = window.location.search;
+//   let urlParams = new URLSearchParams(queryString);
+//   let productId = urlParams.get('id');
+
+// let cardsFetch = function () {
+//     fetch("http://localhost:3000/api/products/"+productId)
+//     .then((response) => response.json())
+//     .then((product) => {
+  
+//     console.log(product);
+
+
+//    // ajout des cards sur la page produit 
+//     document.getElementsByClassName("item__img")[0].innerHTML = 
+//     '<img src="'+product.imageUrl+'" alt="Photographie d\'un canapé">';
+//     document.getElementById("title").innerHTML = product.name;
+//     document.getElementById("price").innerHTML = product.price;
+//     document.getElementById("description").innerHTML = product.description;
+    
+
+//  //fonction pour la gestion du panier en local storage
+//     function ItemPanier(productId, quantity, color){
+//     if(!localStorage.getObj("panier")){
+//         localStorage.setObj("panier",[]);
+//     }
+//     let panier= localStorage.getObj("panier");
+// //vérifie l'id de l'url pour enregistré le bon produit avec le bonne id (Gerer la quantité)
+//     if(quantity=>1){
+//     let quantityColor= false
+//     for (let i=0; i<panier.length; i++){
+//     if(panier[i].id === productId){
+//     quantityColor = i;
+//  }
+//     }
+//     //Gestion de la couleur et sa quantité
+//     if(quantityColor!==false){
+//     if(panier[quantityColor].color === color){
+//     panier[quantityColor].quantity = parseInt(panier[quantityColor].quantity) + parseInt(quantity);
+//     }
+//     else{
+//     panier.push({"id":productId,"quantity":quantity,"color":color});
+//   }
+//     }
+//      }
+//       localStorage.setObj("panier",panier);
+//     }
+
+//     //envoie les différentes couleurs des canapés
+//     if(product!==false){
+//       for (let j=0; j<product.colors.length; j++){
+//         document.getElementById("colors").innerHTML+=
+//         '<option value="'+product.colors[j]+'">'+product.colors[j]+'</option>';
+//       }
+//       // buton au clic redirectionne à la pag panier 
+//        document.getElementById("addToCart").addEventListener("click", event=>{
+//         ItemPanier(product._id, document.getElementById("quantity").value, document.getElementById("colors").value);
+//         window.location = "./cart.html"
+//       });
+      
+//     }else{
+//         // gère la redirection si produit inexistant
+//         window.location = "./index.html"
+//       }
+ 
+
+//     });
+// };
+    
+// cardsFetch();
